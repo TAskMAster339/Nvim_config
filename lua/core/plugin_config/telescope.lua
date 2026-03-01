76a944a5b826d7f1f487efeb995cc1404d020ead
TAskMAster339 <dimagr3214@gmail.com>
2026-03-01 21:17:43 +0300

    fix: telescope

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,3 +1,11 @@
+-- Compatibility fix: newer nvim-treesitter removed ft_to_lang
+if vim.treesitter.language.ft_to_lang == nil then
+  vim.treesitter.language.ft_to_lang = function(ft)
+    local ok, lang = pcall(vim.treesitter.language.get_lang, ft)
+    return ok and lang or ft
+  end
+end
+
 vim.keymap.set('n', '<c-p>', require('telescope.builtin').find_files, {})
 vim.keymap.set('n', '<Space><Space>', require('telescope.builtin').oldfiles, {})
 vim.keymap.set('n', '<Space>fg', require('telescope.builtin').live_grep, {})
