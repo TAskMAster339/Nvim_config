d6e7e5a7a235f57e54b6d527e63b5da4969bb39b
TAskMAster339 <dimagr3214@gmail.com>
2026-03-01 21:00:21 +0300

    fix: preload bug

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -12,18 +12,43 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   --- Colorscheme
-  { "ellisonleao/gruvbox.nvim" },
-  { "folke/tokyonight.nvim" },
-  { "rebelot/kanagawa.nvim" },
+  { "ellisonleao/gruvbox.nvim", lazy = true },
+  { "folke/tokyonight.nvim",   lazy = true },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("core.plugin_config.gruvbox")
+    end,
+  },
 
   --- File tree
-  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("core.plugin_config.nvim-tree")
+    end,
+  },
 
   --- Status line
-  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("core.plugin_config.lualine")
+    end,
+  },
 
   --- Syntax highlighting
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("core.plugin_config.treesitter")
+    end,
+  },
 
   --- LSP / Mason
   {
@@ -31,7 +56,11 @@ require("lazy").setup({
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
       "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
     },
+    config = function()
+      require("core.plugin_config.lsp_config")
+    end,
   },
 
   --- Completion
@@ -42,33 +71,72 @@ require("lazy").setup({
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
     },
+    config = function()
+      require("core.plugin_config.completions")
+    end,
   },
 
   --- Bufferline
-  { "akinsho/bufferline.nvim", version = "*", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("core.plugin_config.buffer")
+    end,
+  },
 
   --- Todo comments
-  { "folke/todo-comments.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "folke/todo-comments.nvim",
+    version = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("core.plugin_config.todo")
+    end,
+  },
 
   --- Telescope
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("core.plugin_config.telescope")
+    end,
   },
 
   --- Auto pairs
-  { "windwp/nvim-autopairs" },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("core.plugin_config.autopairs")
+    end,
+  },
 
   --- Comments
-  { "numToStr/Comment.nvim" },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("core.plugin_config.comment")
+    end,
+  },
 
   --- Highlighting
-  { "RRethy/vim-illuminate" },
+  {
+    "RRethy/vim-illuminate",
+    config = function()
+      require("core.plugin_config.illuminate")
+    end,
+  },
 
   --- Better escape
-  { "max397574/better-escape.nvim" },
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("core.plugin_config.better-escape")
+    end,
+  },
 }, {
-  -- lazy.nvim options
   ui = { border = "rounded" },
 })
